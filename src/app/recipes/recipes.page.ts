2ef27c8b36a4f695090f3a5276b5c6f73a45a372
Kamal Norizan <kamalnorizan@gmail.com>
2019-10-02 14:37:33 +0800

    Service

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipesService } from './recipes.service';
 
 @Component({
   selector: 'app-recipes',
@@ -7,22 +8,10 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-  recipes: Recipe[] = [
-    {
-      id: 'r1',
-      title: 'Mee Goreng',
-      imageUrl: 'https://iresipi.com/media/DSC_5402.jpg',
-      ingredients: ['Mee Kuning', 'Sayur Sawi', 'Cili', 'Daun Bawang', 'Udang', 'Limau Kasturi']
-    },
-    {
-      id: 'r2',
-      title: 'Nasi Lemak',
-      imageUrl: 'https://d1alt1wkdk73qo.cloudfront.net/images/guide/6abb55670f06cebacdc5023d11367153/640x478_ac.jpg',
-      ingredients: ['Nasi', 'Santan', 'Ikan Bilis', 'Timun', 'Lada Kering', 'Kacang Tanah', 'Telur Rebus']
-    }
-  ];
-  constructor() { }
+  recipes: Recipe[];
+  constructor(private recipesService: RecipesService) { }
   ngOnInit() {
+    this.recipes = this.recipesService.getAllRecipes();
   }
 
 }
---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  private recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Mee Goreng',
+      imageUrl: 'https://iresipi.com/media/DSC_5402.jpg',
+      ingredients: ['Mee Kuning', 'Sayur Sawi', 'Cili', 'Daun Bawang', 'Udang', 'Limau Kasturi']
+    },
+    {
+      id: 'r2',
+      title: 'Nasi Lemak',
+      imageUrl: 'https://d1alt1wkdk73qo.cloudfront.net/images/guide/6abb55670f06cebacdc5023d11367153/640x478_ac.jpg',
+      ingredients: ['Nasi', 'Santan', 'Ikan Bilis', 'Timun', 'Lada Kering', 'Kacang Tanah', 'Telur Rebus']
+    }
+  ];
+  constructor() { }
+
+  getAllRecipes() {
+    return [...this.recipes];
+  }
+
+  getRecipe() {
+
+  }
+}
