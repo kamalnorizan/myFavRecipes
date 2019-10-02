72a7ec0f37413ebe2bd1a2d5625b17222e62e02c
Kamal Norizan <kamalnorizan@gmail.com>
2019-10-02 12:27:49 +0800

    Contoh konsep GET

--- src/app/recipe-detail/recipe-detail.page.ts
+++ src/app/recipe-detail/recipe-detail.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-detail.page.scss'],
 })
 export class RecipeDetailPage implements OnInit {
+  recipeId = '';
+  title = '';
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      this.recipeId = paramMap.get('recipeId');
+      this.title = paramMap.get('title');
+    });
   }
 
 }
